b1fa9f5757bd6cf1f7658090857d7aa44a2e750e
Pedro Rabis <rabis.pedro@gmail.com>
2020-03-19 08:49:13 -0300

    Soma de Ímpares Consecutivos II

---
+++ Iniciante/C++/1099.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int N;
+    scanf("%d",&N);
+    for(int i=0;i<N;i++){
+        int maior,menor,x,y,soma=0;
+        scanf("%d %d",&x,&y);
+        if(x>y){
+            maior=x;
+            menor=y;
+        }else{
+            maior=y;
+            menor=x;
+        }
+        for(int j=menor+1;j<maior;j++) if(j%2) soma+=j;
+        printf("%d\n",soma);
+    }
+    return 0;
+}
