9183791264d30fb52bd5efbe839a5bcb09c7b878
Pedro Rabis <rabis.pedro@gmail.com>
2020-03-19 08:35:03 -0300

    Senha Fixa

---
+++ Iniciante/C++/1114.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    string s;
+    while(true){
+        cin>>s;
+        if(s!="2002") cout<<"Senha Invalida\n";
+        else break;
+    }
+    cout<<"Acesso Permitido\n";
+    return 0;
+}
